1aa4a6d4c1cde7f9731997e00828339fe63e88e7
Chaudhari Rohan <134542326+letsCodeWithRohan@users.noreply.github.com>
2024-02-26 09:26:46 +0530

    Add files via upload

--- ConstOver.java
+++ ConstOver.java
@@ -1,14 +1,25 @@
 class ConstOver{
+	String name;
+	
 	ConstOver(){
 		System.out.println("Default Constructor !");
+		name = "Default";
 	}
 	
 	ConstOver(String name){
+		this.name = name;
 		System.out.println("Parametrized Constructor by "+name+" !");
 	}
+	
+	ConstOver(ConstOver c){
+		name = c.name;
+		System.out.println("Copy Constructor By Name of "+name+" !");
+	}
+	
 	public static void main(String args[]){
-		ConstOver obj1,obj2;
+		ConstOver obj1,obj2,obj3;
 		obj1 = new ConstOver();
 		obj2 = new ConstOver("Rohan");
+		obj3 = new ConstOver(obj1);
 	}
 }
