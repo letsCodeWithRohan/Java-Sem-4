de7e32c63b29fa8e65713694c4059faa33670019
Chaudhari Rohan <134542326+letsCodeWithRohan@users.noreply.github.com>
2024-03-22 14:26:32 +0530

    Add files via upload

---
+++ myPackage.java
@@ -0,0 +1,22 @@
+package myPack;
+
+public class myPackage{
+	public static void main(String args[]){
+		System.out.println("This is package created by Rohan");
+	}
+	public int sum(int a,int b){
+		return (a + b);
+	}
+	public int minus(int a,int b){
+		return (a - b);
+	}
+	public int multiply(int a,int b){
+		return a * b;
+	}
+	public int divide(int a,int b){
+		return a / b;
+	}
+	public int mod(int a,int b){
+		return a % b;
+	}
+}
